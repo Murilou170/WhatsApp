0c3249a02341767a6bf01848bdd98482a5ba62ba
Murilo Rodrigues <43734393+Murilou170@users.noreply.github.com>
2022-03-18 19:34:59 -0300

    enviando mensagem

--- lib/Mensagens.dart
+++ lib/Mensagens.dart
@@ -5,11 +5,9 @@ import 'model/Usuario.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Mensagens extends StatefulWidget {
-
   late Usuario contato;
 
   Mensagens(this.contato);
-
 
   @override
   State<Mensagens> createState() => _MensagensState();
@@ -18,56 +16,54 @@ class Mensagens extends StatefulWidget {
 class _MensagensState extends State<Mensagens> {
 
   late String _idUsuarioLogado;
+  late String _idUsuarioDestinatario;
 
-List<String> listaMensagens = [
+  FirebaseFirestore db = FirebaseFirestore.instance;
 
-
-];
+  List<String> listaMensagens = [];
   TextEditingController _controllerMensagem = TextEditingController();
-  _enviarMensagem(){
 
+  _enviarMensagem() {
     String textoMensagem = _controllerMensagem.text;
-    if(textoMensagem.isNotEmpty){
-
-
-
+    if (textoMensagem.isNotEmpty) {
       Mensagem mensagem = Mensagem();
       mensagem.idUsuario = _idUsuarioLogado;
-      mensagem.mensagem  = textoMensagem;
+      mensagem.mensagem = textoMensagem;
       mensagem.urlImagem = "";
-      mensagem.tipo      = "texto";
+      mensagem.tipo = "texto";
 
-      _salvarMensagem();
-
+      _salvarMensagem(_idUsuarioLogado, _idUsuarioDestinatario, mensagem);
     }
-
   }
 
-  _salvarMensagem(){
+  _salvarMensagem(String idRemetente, String idDestinatario, Mensagem msg) async {
+    await db
+        .collection("mensagens")
+        .doc(idRemetente)
+        .collection(idDestinatario)
+        .add(msg.toMap());
 
-
-
+    //limpar o texto
+    _controllerMensagem.clear();
   }
 
-  _enviarFoto(){}
+  _enviarFoto() {}
 
-    _recuperarDadosUsuario() async{
-
-      var usuarioLogado = FirebaseAuth.instance.currentUser;
+  _recuperarDadosUsuario() async {
+    var usuarioLogado = FirebaseAuth.instance.currentUser;
     _idUsuarioLogado = usuarioLogado!.uid;
 
-}
-@override
+    _idUsuarioDestinatario = widget.contato.idUsuario;
+  }
+
+  @override
   void initState() {
     super.initState();
     _recuperarDadosUsuario();
   }
 
-
-
   @override
   Widget build(BuildContext context) {
-
     var caixaMensagem = Container(
       padding: EdgeInsets.all(8),
       child: Row(
@@ -76,31 +72,28 @@ List<String> listaMensagens = [
             child: Padding(
               padding: EdgeInsets.only(right: 8),
               child: TextField(
-                controller: _controllerMensagem,
-                autofocus: true,
-                keyboardType: TextInputType.text,
-                style: TextStyle(fontSize: 20),
-                decoration: InputDecoration(
-
-                  contentPadding: EdgeInsets.fromLTRB(32, 8, 32, 8),
-                  hintText: "Digite uma mensagem...",
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(4.0)
-                   ),
-              prefixIcon: IconButton(
-                  icon: Icon(Icons.camera_alt_outlined),
-                  onPressed: _enviarFoto
-              )
-                  )
-                ),
-              ),
+                  controller: _controllerMensagem,
+                  autofocus: true,
+                  keyboardType: TextInputType.text,
+                  style: TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.fromLTRB(32, 8, 32, 8),
+                      hintText: "Digite uma mensagem...",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(4.0)),
+                      prefixIcon: IconButton(
+                          icon: Icon(Icons.camera_alt_outlined),
+                          onPressed: _enviarFoto))),
             ),
-
+          ),
           FloatingActionButton(
             backgroundColor: Color(0xff075E54),
-            child: Icon(Icons.send_outlined, color: Colors.lightBlueAccent,),
+            child: Icon(
+              Icons.send_outlined,
+              color: Colors.lightBlueAccent,
+            ),
             mini: true,
             onPressed: _enviarMensagem,
           )
@@ -108,55 +101,116 @@ List<String> listaMensagens = [
       ),
     );
 
-    var listView = Expanded(child: ListView.builder(itemCount: 2,
-        itemBuilder: (context, indice){
-
-      double larguraContainer = MediaQuery.of(context).size.width * 0.8;
-
-        return Align(
-          alignment: Alignment.centerRight,
-          child: Padding(
-            padding: EdgeInsets.all(6),
-            child: Container(
-              width: larguraContainer,
-              padding: EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.all(Radius.circular(8))
+    var stream = StreamBuilder(
+        stream: db
+            .collection("mensagens")
+            .doc(_idUsuarioLogado)
+            .collection(_idUsuarioDestinatario)
+            .snapshots(),
+        builder: (context, snapshot){
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+          case ConnectionState.waiting:
+            return Center(
+              child: Column(
+                children: [
+                  Text("Carregando mensagens"),
+                  CircularProgressIndicator()
+                ],
               ),
-              child: Text(listaMensagens[indice] ),
-            ),
-          ),
-        );
+            );
+
+          case ConnectionState.active:
+          case ConnectionState.done:
+            QuerySnapshot? querySnapshot = snapshot.data as QuerySnapshot<Object?>?;
+
+            if (snapshot.hasError) {
+              return Expanded(
+                child: Text("Erro ao carregar os dados!"),
+              );
+
+            } else {
+              return Expanded(
+                child: ListView.builder(
+                    itemCount: querySnapshot!.docs.length,
+                    itemBuilder: (context, indice) {
+
+                      //recupera mensagem
+                      List<DocumentSnapshot> mensagens = querySnapshot.docs.toList();
+                      DocumentSnapshot item = mensagens[indice];
+
+
+                      double larguraContainer =
+                          MediaQuery.of(context).size.width * 0.8;
+
+
+                      return Align(
+                        alignment: Alignment.centerRight,
+                        child: Padding(
+                          padding: EdgeInsets.all(6),
+                          child: Container(
+                            width: larguraContainer,
+                            padding: EdgeInsets.all(16),
+                            decoration: BoxDecoration(
+                                color: Colors.blue,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(8))),
+                            child: Text(item["mensagem"]),
+                          ),
+                        ),
+                      );
+                    }),
+              );
+            }
+
         }
-        ),
+      },
+      );
+
+
+    var listView = Expanded(
+      child: ListView.builder(
+          itemCount: 2,
+          itemBuilder: (context, indice) {
+            double larguraContainer = MediaQuery.of(context).size.width * 0.8;
+
+            return Align(
+              alignment: Alignment.centerRight,
+              child: Padding(
+                padding: EdgeInsets.all(6),
+                child: Container(
+                  width: larguraContainer,
+                  padding: EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.all(Radius.circular(8))),
+                  child: Text(listaMensagens[indice]),
+                ),
+              ),
+            );
+          }),
     );
 
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xff075E54),
-        title: Text(widget.contato.nome),
-      ),
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("imagens/bg.png"),
-          fit: BoxFit.cover
-        )
+        appBar: AppBar(
+          backgroundColor: Color(0xff075E54),
+          title: Text(widget.contato.nome),
         ),
-        child: SafeArea(
-          child: Container(
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("imagens/bg.png"), fit: BoxFit.cover)),
+          child: SafeArea(
+              child: Container(
             padding: EdgeInsets.all(16),
             child: Column(
-            children: [
-              listView,
-              caixaMensagem,
-            ],
-          ),
-        )
-      ),
-    )
-    );
+              children: [
+                stream,
+                caixaMensagem,
+              ],
+            ),
+          )),
+        ));
   }
 }
